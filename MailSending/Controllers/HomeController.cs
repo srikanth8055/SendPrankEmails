915018f7fe3b591094d410a129c418a7d3c9bf0c
Srikanth Puli <25710514+srikanth8055@users.noreply.github.com>
2018-08-04 17:28:52 +0530

    simple textbox and button
    no much implementation. only basic code for sending..and no design of page...

---
+++ MailSending/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using MailSending.Models;
+
+namespace MailSending.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void SendMessage(UserDetails objData)
+        {
+            MailMessage msg = new MailMessage("hogriders8055@gmail.com", "hogrider8055@gmail.com");
+
+            msg.Subject = objData.Subject;
+            msg.Body = objData.BodyMessage;
+
+            SmtpClient smpt = new SmtpClient("smtp.gmail.com",587);
+            smpt.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "hogriders8055@gmail.com",
+                Password = "srikanth@1234"
+            };
+            smpt.EnableSsl = true;
+            smpt.Send(msg);
+        }
+    }
+}
---
+++ MailSending/Models/UserDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MailSending.Models
+{
+    public class UserDetails
+    {
+        public string Subject { get; set; }
+        public string BodyMessage { get; set; }
+
+    }
+}
